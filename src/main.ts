77e570d4ade0b61d4626ef14c838166f4366dd2f
Petar Jakic <petar.jakic.993@gmail.com>
2020-05-13 17:28:36 +0200

    Implement staticlly server photos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { StorageConfig } from 'config/storage.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(StorageConfig.photo.destination, {
+    prefix: StorageConfig.photo.urlPrefix,
+    maxAge: StorageConfig.photo.maxAge,
+    index: false,
+  })
+
   await app.listen(3000);
 }
 bootstrap();
